5854ecb95829b31bd0df76d5e738be4f4c63d408
xithan <philipp.kuehl@gmail.com>
2014-05-18 14:18:01 +0200

    initial commit

---
+++ convert.rb
@@ -0,0 +1,311 @@
+# Erstellt fuer jede Definition und jeden Raum eine eigene PDF-Datei
+
+require 'byebug'
+
+# paths for input data
+$indir = "tex/"
+$bibfile = "example"
+$infile = "example"
+$bibfile = "example"
+$packagedir = "packages/"
+
+$workdir = "tmp/"
+
+# paths for output data
+$mainfile = "main"
+$outdir = "pdfviewer/web/pdf/"
+$bibdir = "pdfviewer/web/pdf/bib/"
+$bibstyle = "alpha"
+
+
+
+$glos = {}
+
+$no_ref = ARGV.include?("-no-ref")
+$no_gls = ARGV.include?("-no-gls")
+$no_sec = ARGV.include?("-no-sec")
+$no_typeset = ARGV.include?("-no-typeset")
+$no_cleanup = ARGV.include?("-no-cleanup")
+
+def readtex(file)
+  filestack = [File.new($indir+file+".tex","r")]
+  $documentclass =  filestack.last.readline()
+  raise TeXError unless /\\documentclass/.match($documentclass)
+  document = false
+  comment = false
+  while not filestack.empty?
+    begin
+      line = filestack.last.readline()
+      if /^\s*\\end\{markas\}/.match(line)
+        comment = false
+      elsif /^\s*%.*$/.match(line) or comment
+        next
+      elsif /\\bibliography/.match(line)
+        next 
+      elsif /^\s*\\begin\{markas\}/.match(line)
+        comment = true
+      elsif /^\s*\\begin\{document\}/.match(line)
+        document = true
+        yield(line)
+      elsif sub = /\\(?:input|include)\{([^}]+)\}/.match(line) #and document
+        filestack.push(File.new($indir+sub[1] + ".tex","r"))
+      else
+        yield(line)
+      end
+    rescue EOFError
+      filestack.pop.close()
+    end
+  end
+end
+
+
+def parse_preamble(line)
+  $preamble_file.write(line)
+end
+
+def parse_room(line)
+  $target.write(line)
+  if entry = /^\s*\\\w*nodei?[^{\s]*\{([^}]+)\}/.match(line) 
+    $glos[entry[1]] = true
+  end
+end
+
+def parse_emergency(line)
+  if entry = /^\s*\\\w*nodei?[^{\s]*\{([^}]+)\}/.match(line) 
+    $glos[entry[1]] = true
+  end
+end
+
+def new_texfile(filename, type)
+  file = File.open($workdir+filename+".tex", "w") # Alles weitere in die Hauptdatei
+  file.puts($documentclass)
+  file.puts('\include{preamble}')
+  if type == :mathview
+    file.puts('\mathview')
+  elsif type == :defview
+    file.puts('\defview')
+  end
+  file.puts("\\bibliographystyle{#{$bibstyle}}")
+  file.puts('\begin{document}')
+#  file')\\nobibliography{#{$bibfile}}
+#file.puts("\\newsavebox\\mytempbib
+# => \\savebox\\mytempbib{\\parbox{\\textwidth}{\\bibliography{#{$bibfile}}}}")
+  file.puts("\\nobibliography{#{$bibfile}}")
+#file.puts('\noampel')
+#  file.puts('\pagestyle{fancy}')
+  return file
+end
+
+
+def close_file(file)
+  #file.puts "\\begingroup\\renewcommand{\\chapter}[2]{}\\endgroup"
+  file.puts '\end{document}'
+  file.close
+end
+
+def cleanup name
+    #system( "cd #{$outdir}; find . -type f -name '#{name}.*' -not -name '*.pdf' -not -name '*.tex' | xargs rm")
+end
+
+def run_initex filename
+  #puts "##########################################"
+  puts "Run initex #{filename}"
+  return system( "cd #{$workdir}; pdfetex '-ini' '-interaction=batchmode' '-file-line-error' '&pdflatex' './#{filename}.ini' >/dev/null" )
+end
+
+def run_bibtex name, workdir = $workdir
+  #puts "##########################################"
+  puts "Run bibtex #{name}"
+  return system( "cd #{workdir}; bibtex #{name}") #"  >/dev/null" )
+end
+
+def run_latex name, format=:latex, workdir = $workdir
+  #puts "##########################################"
+  case format
+  when :latex
+    puts "Run latex #{workdir}#{name}"
+    return system("cd #{workdir}; latex './#{name}.tex'")#" >/dev/null")
+  when :pdflatex 
+    puts "Run pdflatex #{workdir}#{name}"
+    return system("cd #{workdir}; pdflatex './#{name}.tex'")#{}" >/dev/null")
+  else
+    puts "Run pdfetex #{workdir}#{name}"
+    #return system("cd #{workdir}; pdfetex -output-format pdf '-interaction=batchmode' -file-line-error -synctex=0 -fmt '#{format}'  './#{name}.tex' ")#>/dev/null")
+    puts "cd #{workdir}; pdfetex -output-format pdf  -file-line-error -synctex=0 -fmt '#{format}'  './#{name}.tex' "
+    return system("cd #{workdir}; pdfetex -output-format pdf  -file-line-error -synctex=0 -fmt '#{format}'  './#{name}.tex' ")
+  end
+end
+
+def move_file filename, ending, outdir=$outdir
+  puts "moved #{$workdir}#{filename}.#{ending} to #{outdir}#{filename}.#{ending}"
+  FileUtils.cp("#{$workdir}#{filename}.#{ending}", "#{outdir}#{filename}.#{ending}")
+end
+
+def move_pdf filename, outdir=$outdir
+  move_file filename, "pdf", outdir
+end
+
+def move_svg filename, outdir=$outdir
+  move_file filename, "svg", outdir
+end
+
+def run_dvitosvg name, workdir = $workdir
+  return system("cd #{workdir}; dvisvgm -n './#{name}'")
+end
+
+def typeset(file = $outfile, format = :mathview)
+  if (format == :defview and $no_gls) or $no_typeset or (format == :mathview and $no_sec)
+    return
+  end
+  puts "##########################################"
+  puts "Process #{file}"
+  run_latex  file, format.to_s
+ # run_latex  file, format.to_s
+  #system("cd #{$outdir} ; pdfetex -output-format pdf -interaction batchmode -synctex=0 -fmt #{format.to_s}  './#{file}.tex'")
+  if format == :mathview
+    run_bibtex file
+    #run_latex  file, format.to_s
+  #  run_bibtex file
+    run_latex  file, format.to_s
+    run_latex  file, format.to_s
+    #system("cd #{$outdir} ; pdfetex -output-format pdf -interaction batchmode -synctex=0 -fmt #{format.to_s}  './#{file}.tex'")
+  end
+  cleanup file unless $no_cleanup
+  #system("cd #{$outdir} && pdflatex #{file} -halt-on-error")
+end
+
+def typeset_preamble workdir=$workdir
+  puts "##########################################"
+  puts "Process preamble"
+  File.open(workdir+"defview.ini", "w") { |file|  # Alles weitere in die Hauptdatei
+    file.puts($documentclass)
+    file.puts('\input{preamble}')
+    file.puts('\defview')
+    file.puts('\dump')
+  }
+  
+  File.open(workdir+"mathview.ini", "w") { |file| # Alles weitere in die Hauptdatei
+    file.puts($documentclass)
+    file.puts('\input{preamble}')
+    file.puts('\mathview')
+    file.puts('\dump')
+  }
+  
+  # Die beiden nachfolgenden auskommentireten funktionieren auf jeden Fall.
+  # run_initex ist noch ungetest
+  #system("perl '#{$indir}generate_format.pl' '#{$outdir}mathview.tex' --fmt=mathview")
+  #system("perl '#{$indir}generate_format.pl' '#{$outdir}defview.tex' --fmt=defview")
+  run_initex "mathview"
+  run_initex "defview"
+#  system("cd #{$indir} ; perl generate_format.pl #{$outdir}mathview.tex --fmt=#{$outdir}mathview")
+  
+#  Dir.chdir("/Users/drkuehl/Dropbox/Doktorarbeit/TsoReordered/Thesis/pdfjs/thesis")
+#  system("pdfetex -output-format pdf -ini -interaction batchmode -synctex=0 -recorder -file-line-error &pdflatex './defview.ini'")
+  
+  # system("cd #{$outdir} ; pdfetex -output-format pdf -ini -interaction batchmode -synctex=0 -recorder -file-line-error &pdflatex './mathview.ini'")
+end
+
+def bibitem_template bibitem
+  %{\\documentclass[10pt]{article}
+\\usepackage{#{$packagedir}screenread}
+\\usepackage{hyperref}
+\\renewcommand\\refname{\\vspace{-1cm}}
+\\begin{document}
+  \\nocite{*}
+  \\begin{thebibliography}{1}
+  #{bibitem}
+  \\end{thebibliography}
+\\end{document}
+  }
+end
+
+def typeset_references
+  run_latex $infile, :latex
+  run_bibtex $infile
+  bibitems = File.read($workdir+$infile+".bbl").scan /(\\bibitem(?:\[[^\]]+\])?\{([^}]+)\}.+?\n\n)/m
+  bibitems.each do |ref|
+    File.open($workdir+ref[1]+".tex","w") { |f| f.puts( bibitem_template(ref[0]) )}
+    unless $no_typeset
+      run_latex ref[1], :latex 
+      run_dvitosvg ref[1]
+      move_svg ref[1], $bibdir
+    end
+  end
+end
+
+
+FileUtils.copy_entry $indir, $workdir
+
+  #puts ARGV.inspect
+if not $no_ref
+  puts "Typeset references"
+  typeset_references
+end
+
+$room_cnt = 0
+parse = :parse_preamble
+$preamble_file = File.open($workdir+"preamble.tex","w")
+
+readtex($infile) do |line|
+  if /\\begin\{document\}/.match(line)
+    $preamble_file.close() # Praeamble zuende
+    typeset_preamble()
+    parse = nil
+    #$target = new_texfile($outfile,:mathview)
+    #parse = :parse_room
+  elsif /\\end\{document\}/.match(line)
+    close_file($target)
+    typeset($outfile)
+    move_pdf($outfile)
+  elsif room = /^\s*(?:\\room|\\setroom|roomprooftree|roomproofonly(?:noqed)?\})\s*\{([^}]+)\}/.match(line) 
+    $room_cnt += 1
+    if $room_cnt >= 3
+      close_file($target)
+      typeset($outfile)
+      move_pdf($outfile)
+    end
+    if $room_cnt >= 2
+      $outfile = room[1]
+      $target = new_texfile($outfile,:mathview)
+      $target.write(line)
+      parse = :parse_room
+    end
+  elsif /^\s*\\(?:emergency|roomservice)/.match(line)
+    parse = :parse_emergency
+  else
+    send(parse,line) if parse
+  end
+end
+
+
+main_file = new_texfile($mainfile,:mathview)
+File.read($workdir+$infile+".tex").scan(/\\begin\{document\}(.*)\\end\{document\}/m)
+main_document = $1
+main_file.write($1)
+close_file(main_file)
+typeset($mainfile)
+move_pdf($mainfile)
+
+
+
+puts $glos.inspect
+
+unless $no_gls
+  $glos.each_key do |key|
+    file = new_texfile(key, :defview)
+    file.write('\begin{treedef}\rootnode{'+key+'}\end{treedef}\end{document}')
+    file.close
+    typeset(key, :defview)
+    system("cd #{$workdir}; pdfcrop --margins 5 #{key}.pdf #{key}.pdf")
+    move_pdf key
+  end
+end
+
+unless $no_cleanup
+  puts "clean up"
+  FileUtils.rm_rf($workdir)
+  # ['log','aux','bbl','dvi','out','tex','maf'].each do |t|
+  #   system("cd #{$workdir}; find . -iname '*.#{t}' -exec rm {} \\;")
+  # end
+end
+
